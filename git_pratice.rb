ac7707251673ca954b20b00d7a439036056d879c
gracias kalema <graciasgray@gmail.com>
2018-09-25 22:53:41 +0000

    initial commit

---
+++ git_pratice.rb
@@ -0,0 +1,27 @@
+puts "Tell me your name"
+name =gets.chomp()
+puts "Hi #{name} , my name is code the dream"
+
+puts "Tell me a number"
+x = gets.chomp()
+
+puts " great!Tell me another number"
+y =gets.chomp()
+puts "what is 2[x]-[y]*hours of class attended /number of class mates"
+answer=gets.chomp.to_f
+
+#arrays
+games_rock ="nbak19","gta v","call of duty"
+puts games_rock# loops
+
+command = ''
+
+while command != 'bye'
+
+  puts command
+
+  command = gets.chomp
+
+end
+
+puts 'Come again soon!'
